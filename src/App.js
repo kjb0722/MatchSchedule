3a0de5b962a66fbf54dc0443fbfc7f0a65c76b22
kjb0722 <wnswnsqja@naver.com>
2020-11-14 01:04:41 +0900

    css 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import logo from './img/logo_1.png';
 import './App.css';
 import AppMenu from './ui/AppMenu.jsx';
--- src/ui/MainList.jsx
+++ src/ui/MainList.jsx
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
+import '../css/MainList.css';
 
 class MainList extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <p>11월 1일(일)</p>
+        <div className="list">
+          <div>프리미어리그</div>
+          <span>00:00</span>
+          <span>Turf Moor</span>
+          <span>번리</span>
+          <span>0</span>
+          <span>:</span>
+          <span>3</span>
+          <span>첼시</span>
+          <div>경기기록</div>
+        </div>
+      </div>
+    );
   }
 }
 
